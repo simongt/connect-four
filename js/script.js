65d45a910ce866d2eba185c1926ecab9676a438e
Simon G Tsegay <simon.tsegay@gmail.com>
2018-05-10 12:28:58 -0400

    Cleaned up the css layout and formatting

--- js/script.js
+++ js/script.js
@@ -16,26 +16,59 @@ $(function () {
     one: {
       spaces: [],
       combos: [],
-      wins: score.player1 // do not assume this is 0
+      wins: score.player1
     },
     two: {
       spaces: [],
       combos: [],
-      wins: score.player2 // do not assume this is 0
+      wins: score.player2
     }
   };
 
   // grab the html body
   const $body = $('body');
-  // insert game name as header
-  const $nameOfTheGame = $('<h1>').addClass('reduceBottomMargin').html(`Connect 4`).appendTo($body);
-  const $gameStatus1 = $('<p>').addClass('status').html(`Drop it like it's hot!`).appendTo($body);
-  const $board = $('<div>').addClass('board').appendTo($body);
-  let boardSpaces = [];
-  let boardColumns = [];
-  let firstAvailableRow = []; // per row
+  // const $header = $('header').appendTo($body);
+  // const $main = $('main').appendTo($body);
+  // const $footer = $('footer').appendTo($body);
 
-  for(let i = 0; i < 49; i++) {
-    const $space = $('<div>').addClass('space').appendTo($board);
+  // insert game title
+  const $gameTitle = $('<h1>');
+  $gameTitle.addClass('neon');
+  $gameTitle.html(`Connect 4`);
+  $gameTitle.appendTo($body);
+  
+  // insert message below game title
+  const $message = $('<p>');
+  $message.addClass('message');
+  $message.html(`Drop it like it's hot!`);
+  $message.appendTo($body);
+
+  // insert the full board (includes the preview row + visible playable board)
+  const $fullBoard = $('<div>');
+  $fullBoard.addClass('fullBoard');
+  $fullBoard.appendTo($body);
+  const $previewRow = $('<div>');
+  $previewRow.addClass('previewRow');
+  $previewRow.appendTo($fullBoard);
+  const $board = $('<div>');
+  $board.addClass('board');
+  $board.appendTo($fullBoard);
+
+  for(let i = 0; i < 7; i++) {
+    // fill the preview row with non-playable spaces
+    const $space = $('<div>');
+    $space.addClass('space preview');
+    $space.appendTo($previewRow);
+    // fill the preview row with non-playable spaces
+    const $downArrow = $('<i>');
+    $downArrow.addClass('fas fa-arrow-down');
+    $downArrow.addClass('icon arrow');
+    $downArrow.appendTo($space);
+  }
+  for (let i = 0; i < 42; i++) {
+    // fill the preview row with non-playable spaces
+    const $space = $('<div>');
+    $space.addClass('space');
+    $space.appendTo($board);
   }
 });
