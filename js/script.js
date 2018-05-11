aa0c8dca70b22b73aa45f312b84162c8d0b10cc9
Simon G Tsegay <simon.tsegay@gmail.com>
2018-05-10 22:23:16 -0400

    Hovering over board shows pulsing token above single column

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(function () {
     player2: 0, // player 2
     ties: 0
   };
-  /**
+  /*
    * 2 players: player.one and player.two
    * - each player will have their chosen spaces pushed into an array
    * - each player will have their wins tracked from round to round
@@ -54,8 +54,46 @@ $(function () {
   $board.addClass('board');
   $board.appendTo($fullBoard);
 
+  // an array that contains 6 elements which are also arrays that each contain 7 elements
+  let board = createArray(6, 7);  /*
+                                   * 2-D array setup: 6 rows, 7 columns
+                                   * -----------------------------------
+                                   *      row: [ 5   4   3   2   1   0 ]
+                                   * column 1: [ 0,  7, 14, 21, 28, 35 ]
+                                   * column 2: [ 1,  8, 15, 22, 29, 36 ]
+                                   * column 3: [ 2,  9, 16, 23, 30, 37 ]
+                                   * column 4: [ 3, 10, 17, 24, 31, 38 ]
+                                   * column 5: [ 4, 11, 18, 25, 32, 39 ]
+                                   * column 6: [ 5, 12, 19, 26, 33, 40 ]
+                                   * column 7: [ 6, 13, 20, 27, 34, 41 ]
+                                   * -----------------------------------
+                                   * array representation by [row][col]
+                                   *  [ 5 ] [  0 ...  6 ]
+                                   *  [ 4 ] [  7 ... 13 ]
+                                   *  [ 3 ] [ 14 ... 20 ]
+                                   *  [ 2 ] [ 21 ... 27 ]
+                                   *  [ 1 ] [ 28 ... 34 ]
+                                   *  [ 0 ] [ 35 ... 41 ]
+                                   */
+  console.log(board);
+  /*
+   * handy "multi-dimensional" array creating function that takes in arguments
+   * courtesy of Matthew Crumley on Stack OverFlow:
+   * https: //stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+   */
+  function createArray(length) {
+    let newArray = new Array(length || 0), i = length;
+    if (arguments.length > 1) {
+      let args = Array.prototype.slice.call(arguments, 1);
+      while (i--) {
+        newArray[length - 1 - i] = createArray.apply(this, args);
+      }
+    }
+    return newArray;
+  }
+
   // fill the board with spaces, fill one preview row above the board
-  for(let i = 0; i < 7; i++) {
+  for (let i = 0; i < 7; i++) {
     // fill the preview row with non-playable spaces
     const $previewSpace = $('<div>');
     $previewSpace.addClass('space preview');
@@ -66,6 +104,9 @@ $(function () {
     $downArrow.addClass('icon arrow');
     $downArrow.appendTo($previewSpace);
   }
+
+  // store the board's elements into the multi-dimensional array
+  // board[row][col]
   for (let i = 0; i < 42; i++) {
     // fill each column with playable spaces
     const $space = $('<div>');
@@ -73,20 +114,5 @@ $(function () {
     $space.html(i);
     $space.appendTo($board);
   }
-
-  /**
-   * 2-D array setup: 6 rows, 7 columns
-   * -----------------------------------
-   *      row: [ 5   4   3   2   1   0 ]
-   * column 1: [ 0,  7, 14, 21, 28, 35 ]
-   * column 2: [ 1,  8, 15, 22, 29, 36 ]
-   * column 3: [ 2,  9, 16, 23, 30, 37 ]
-   * column 4: [ 3, 10, 17, 24, 31, 38 ]
-   * column 5: [ 4, 11, 18, 25, 32, 39 ]
-   * column 6: [ 5, 12, 19, 26, 33, 40 ]
-   * column 7: [ 6, 13, 20, 27, 34, 41 ]
-   * -----------------------------------
-   */
   
-
 });
